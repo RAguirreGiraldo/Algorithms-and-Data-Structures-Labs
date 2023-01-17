44ead07dfe493e2a7f7473362b82939565d6d1e5
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-17 13:56:57 -0600

    Create Excersice 2 List of Numbers and list result is listOfResult

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -5,7 +5,7 @@
 int[] listOfNumber = { 1, 2, 3, 4, 7, 9, 2, 4 };
 int counter;
 
-Console.Write("Numbers that appear twice in the list: ");
+Console.WriteLine("Numbers that appear twice in the list: ");
 
 for (int i = 0; i < listOfNumber.Length; i++)
 {
@@ -25,5 +25,14 @@ for (int i = 0; i < listOfNumber.Length; i++)
     }
 }
 
+/****** Excersice 2 *************/
 
+
+int[] listOfNumber1 = { 1, 2, 3, 4, 5 };
+int[] listOfNumber2 = { 2, 5, 7, 9, 13 };
+int[] listOfResult = new int[listOfNumber1.Length + listOfNumber2.Length];
+
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
 
