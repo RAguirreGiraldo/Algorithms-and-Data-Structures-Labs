7fe47badb307562e2e9240b5746eaa0cf6c2b5ef
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-17 13:06:26 -0600

    Create a Variable bool to check if value is repeat or not. and create a conunter to storage number of time the number is repeat

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -5,11 +5,25 @@
 int[] listOfNumber = { 1, 2, 3, 4, 7, 9, 2, 4 };
 int counter;
 
+Console.Write("Numbers that appear twice in the list: ");
+
 for (int i = 0; i < listOfNumber.Length; i++)
 {
+    bool repeatNumber = false;
     counter = 0;
     for (int j = 0; j < listOfNumber.Length; j++)
     {
-       
+        if (listOfNumber[i] == listOfNumber[j])
+        {
+            counter++;
+        }
+    }
+    if (counter == 2 && repeatNumber == false)
+    {
+        Console.Write(listOfNumber[i] + " ");
+        repeatNumber = true;
     }
 }
+
+
+
