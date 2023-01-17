c4bd3c7c4f32c6c6788a9ef32da3fe5e4cb8619d
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-17 14:12:29 -0600

    Create While to evaluate size of the arrays

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -5,7 +5,7 @@
 int[] listOfNumber = { 1, 2, 3, 4, 7, 9, 2, 4 };
 int counter;
 
-Console.WriteLine("Numbers that appear twice in the list: ");
+Console.Write("Numbers that appear twice in the list : ");
 
 for (int i = 0; i < listOfNumber.Length; i++)
 {
@@ -35,4 +35,11 @@ int[] listOfResult = new int[listOfNumber1.Length + listOfNumber2.Length];
 int counter1 = 0;
 int counter2 = 0;
 int counter3 = 0;
+
+Console.WriteLine(" ");
+
+while (counter1 < listOfNumber1.Length && counter2 < listOfNumber2.Length)
+{
+    
+}
 
