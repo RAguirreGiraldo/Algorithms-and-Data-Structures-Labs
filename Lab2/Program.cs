ebae6b4a92347b56531899236734edafd3ae9053
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-16 19:47:19 -0600

    Create If check current letter, if no alredycheck count and print repeated letters

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -3,20 +3,35 @@
 
 Console.WriteLine("Check if the following string counts repeated words: programmatic python");
 string wordString = "programmatic python";
-char[] repeatedChar = new char[wordString.Length];
+char[] repeatedLetters = new char[wordString.Length];
 int count = 0;
 
 for (int i = 0; i < wordString.Length; i++)
 {
     char currentChar = wordString[i];
-    bool letterChecked = false;
+    bool alreadyChecked = false;
 
-    for (int k = 0; k < i; k++)  //This cicle verify if my code check the letter
+    // Comprobamos si ya hemos revisado la letra
+    for (int k = 0; k < i; k++)
     {
         if (wordString[k] == currentChar)
         {
-            letterChecked = true;
+            alreadyChecked = true;
         }
     }
 
+    if (!alreadyChecked)
+    {
+        for (int j = 0; j < wordString.Length; j++)
+        {
+            if (wordString[i] == wordString[j] && i != j)
+            {
+                repeatedLetters[count] = wordString[i];
+                count++;
+            }
+        }
+    }
 }
+
+Console.WriteLine("Characters that are repeated: " + string.Join(" ", repeatedLetters));
+
