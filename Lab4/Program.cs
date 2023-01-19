c68b951e1b1b8eb45b55993b3fcba7388c64eb4a
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-18 23:00:37 -0600

    create List

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,2 +1,10 @@
-﻿
-/***************  Lab # 4 *******************/
+﻿/***************  Lab # 4 *******************/
+
+/*************** Exercise 1 ****************/
+
+//declaring a list of lists of integers
+List<List<int>> dataList = new List<List<int>>() {
+            new List<int>() { 1, 5, 3 },
+            new List<int>() { 9, 7, 3, -2 },
+            new List<int>() { 2, 1, 2 }
+        };
