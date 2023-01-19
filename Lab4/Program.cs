c45a6f7e1481dd4029745bd809baaba182594f37
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-18 23:31:39 -0600

    Create a for to find the max value of List

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -2,12 +2,25 @@
 
 /*************** Exercise 1 ****************/
 
+ 
 //declaring a list of lists of integers
+List<int> dataListBack= new List<int>();
+
 List<List<int>> dataList = new List<List<int>>() {
             new List<int>() { 1, 5, 3 },
             new List<int>() { 9, 7, 3, -2 },
             new List<int>() { 2, 1, 2 }
         };
 
-//creating a list to store the return values
-List<int> dataListBack= new List<int>();
+for (int i = 0; i < 3; i++)
+{
+    int max = 0;
+    for (int j = 0; j < 3; j++)
+    {
+        if (dataList[i][j] > max)
+            max = dataList[i][j];
+    }
+
+    dataListBack.Add(max);
+}
+
