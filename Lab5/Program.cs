79c8adc89cc27cb9306fb923fad1a98eac4194be
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-22 23:39:08 -0600

    Create opcion1 to add songs

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -2,3 +2,27 @@
 
 // Create a New Playlist
 Queue<string> playlist = new Queue<string>();
+
+Console.WriteLine("Choose an option:");
+Console.WriteLine("1.Add a song to your playlist");
+Console.WriteLine("2. Play the next song in your playlist");
+Console.WriteLine("3. Skip the next song");
+Console.WriteLine("4. Rewind one song");
+Console.WriteLine("5. Exit");
+
+int chooseOpcion = int.Parse(Console.ReadLine());
+
+// Validar la opción del usuario
+if (chooseOpcion  == 1)
+{
+    Console.WriteLine("How Many Song do you want to add");
+    int numSongsadd = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < numSongsadd; i++)
+    {
+        Console.WriteLine("Enter Song Name:");
+        string song = Console.ReadLine();
+        playlist.Enqueue(song);
+        Console.WriteLine($"{song} added to your playlist.");
+    }    
+}
