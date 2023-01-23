4cedc900951ffe485eca53c903398a08fbf3b617
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-01-23 00:46:41 -0600

    Create Option 3 and 4

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -6,6 +6,7 @@ int chooseOpcion;
 
 do
 {
+    Console.WriteLine();
     Console.WriteLine("Choose an option:");
     Console.WriteLine("1.Add a song to your playlist");
     Console.WriteLine("2. Play the next song in your playlist");
@@ -44,6 +45,26 @@ do
         string nextSong = playlist.Peek();
         Console.WriteLine("Next song in the Playlist is : " + nextSong);
 
+    }
+
+
+    if (chooseOpcion == 3)
+    {
+        playlist.Dequeue();
+        Console.WriteLine("Song skipped");
+
+        string nextSong = playlist.Peek();
+        Console.WriteLine("Next song in the Playlist is : " + nextSong);
+    }
+
+    if (chooseOpcion == 4)
+    {
+        string currentSong = playlist.Dequeue();
+        Console.WriteLine("Song rewinded");
+        playlist.Enqueue(currentSong);
+
+        string previousSong = playlist.Peek();
+        Console.WriteLine("Previous song in the Playlist is : " + previousSong);
     }
 
 
